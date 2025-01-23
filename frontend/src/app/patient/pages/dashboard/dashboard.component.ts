e16dfc6165825049b2a8bbd48423af453813d141
ArchitAvd <archit.avadhani@gmail.com>
2025-01-23 23:00:21 +0530

    added patient dashboard page

--- frontend/src/app/patient/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/patient/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import axios from 'axios';
+import {jwtDecode} from 'jwt-decode';
 import { NavbarComponent } from 'src/patient/shared/navbar/navbar.component';
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [NavbarComponent],
+  selector: 'app-patient-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'],
+  imports:[CommonModule,RouterModule,NavbarComponent,]
 })
-export class DashboardComponent {
+export class PatientDashboardComponent implements OnInit {
+  patientName: string | null = null;
+  loading: boolean = true;
+  error: string | null = null;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.fetchUserData();
+  }
+
+  async fetchUserData() {
+    try {
+      this.loading = true;
+      const token = sessionStorage.getItem('token');
+      if (token) {
+        const decodedToken: { username: string } = jwtDecode(token);
+        const username = decodedToken.username;
+        const response = await axios.get(`http://localhost:5000/api/patients/${username}`, {
+          headers: { Authorization: `Bearer ${token}` }
+        });
+        this.patientName = response.data.name;
+      } else {
+        this.router.navigate(['/login']);
+      }
+    } catch (err) {
+      console.log(err);
+      this.error = 'Failed to load patient information';
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  handleGetAppointmentClick() {
+    this.router.navigate(['/patient/appointments']);
+  }
+
+  handleBookAppointmentClick() {
+    this.router.navigate(['/patient/book-appointment']);
+  }
+
+  handleGetMedicalHistory() {
+    this.router.navigate(['/patient/get-history']);
+  }
+
+  handleViewBills() {
+    this.router.navigate(['/patient/showBill']);
+  }
 }
